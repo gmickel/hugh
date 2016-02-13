3b8906fab58bb64e23f831137188988675270ea1
Gordon Mickel <gordon.mickel@gmail.com>
2016-02-13 13:56:28 +0100

    add missing group api methods add method aliases add some jsdoc comments

--- src/index.js
+++ src/index.js
@@ -12,23 +12,14 @@ class HueApi {
       port: port || 80
     };
 
-    this.getLightStatus = this.lightStatus;
-    this.getLights = this.lights;
-  }
-
-  /**
-   *
-   * @param id
-   * @param state
-   * @param options
- * @returns {*}
-   */
-  setLightState(id, state, options = { raw: false }) {
-    return lightsAPI.setLightState(this.config, id, state, options);
-  }
-
-  setGroupState(id, state, options = { raw: false }) {
-    return groupsAPI.setGroupState(this.config, id, state, options);
+    // TODO: Add aliases here named after the official API methods
+    this.getLightAttributesAndState = this.lightStatus;
+    this.getAllLights = this.lights;
+    this.getNewLights = this.newLights;
+    this.searchForNewLights = this.lightSearch;
+    this.setLightAttributes = this.renameLight;
+    this.getAllGroups = this.groups;
+    this.getGroupAttributes = this.groupStatus;
   }
 
   getConfig() {
@@ -39,20 +30,126 @@ class HueApi {
     return lightsAPI.getAllLights(this.config, options);
   }
 
-  lightStatus(id, options = { raw: true }) {
-    return lightsAPI.getLightAttributesAndState(this.config, id, options);
-  }
-
   newLights(options = { raw: true }) {
     return lightsAPI.getNewLights(this.config, options);
   }
 
+  /**
+   * Starts a search for new lights. As of 1.3 will also find switches (e.g. "tap")
+   * @param deviceIds
+   * @param options
+   * @returns {*}
+   */
+  lightSearch(deviceIds = {}, options = { raw: false }) {
+    return lightsAPI.searchForNewLights(this.config, deviceIds, options);
+  }
+
+  /**
+   * Gets the attributes and state of a given light.
+   * @param id
+   * @param options
+   * @returns {*}
+   */
+  lightStatus(id, options = { raw: true }) {
+    return lightsAPI.getLightAttributesAndState(this.config, id, options);
+  }
+
+  /**
+   * Used to rename lights. A light can have its name changed when in any state,
+   * including when it is unreachable or off.
+   * @param id
+   * @param name
+   * @param options
+   * @returns {*}
+   */
   renameLight(id, name, options = { raw: false }) {
     return lightsAPI.setLightAttributes(this.config, id, name, options);
   }
 
-  deleteLight(id, options = { raw: true }) {
+  /**
+   * Allows the user to turn the light on and off, modify the hue and effects
+   * @param id
+   * @param state
+   * @param options
+   * @returns {*}
+   */
+  setLightState(id, state, options = { raw: false }) {
+    return lightsAPI.setLightState(this.config, id, state, options);
+  }
+
+  /**
+   * Deletes a light from the bridge
+   * @param id
+   * @param options
+   * @returns {*}
+   */
+  deleteLight(id, options = { raw: false }) {
     return lightsAPI.deleteLights(this.config, id, options);
+  }
+
+  /* Groups */
+
+  /**
+   * Gets a list of all groups that have been added to the bridge.
+   * A group is a list of lights that can be created, modified and deleted by a user.
+   * The maximum numbers of groups is 16.
+   * @param options
+   * @returns {*}
+   */
+  groups(options = { raw: true }) {
+    return groupsAPI.getAllGroups(this.config, options);
+  }
+
+  /**
+   * Creates a new group containing the lights specified and optional name
+   * @param data
+   * @param options
+   * @returns {*}
+   */
+  createGroup(data, options = { raw: false }) {
+    return groupsAPI.createGroup(this.config, data, options);
+  }
+
+  /**
+   * Gets the name, light membership and last command for a given group.
+   * @param id
+   * @param options
+   * @returns {*}
+   */
+  groupStatus(id, options = { raw: true }) {
+    return groupsAPI.getGroupAttributes(this.config, id, options);
+  }
+
+  /**
+   * Allows the user to modify the name, light and class membership of a group.
+   * @param id
+   * @param groupAttributes
+   * @param options
+   * @returns {*}
+   */
+  setGroupAttributes(id, groupAttributes, options = { raw: false }) {
+    return groupsAPI.setGroupAttributes(this.config, id, groupAttributes, options);
+  }
+
+  /**
+   * Modifies the state of all lights in a group
+   * @param id
+   * @param state
+   * @param options
+   * @returns {*}
+   */
+  setGroupState(id, state, options = { raw: false }) {
+    return groupsAPI.setGroupState(this.config, id, state, options);
+  }
+
+  /**
+   * Deletes the specified group from the bridge.
+   * @param id
+   * @param options
+   * @returns {*}
+   */
+  deleteGroup(id, options = { raw: false }) {
+    return groupsAPI.deleteGroup(this.config, id, options);
   }
 
 }
